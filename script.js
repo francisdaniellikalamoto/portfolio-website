0bf65dbceeca8e4cf6994d949698e82079e9b54b
francisdaniellikalamoto <daniellikalamoto@gmail.com>
2026-05-31 07:17:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Portfolio website loaded successfully");
+fetch('https://portfolio-backend.onrender.com')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+});
+
